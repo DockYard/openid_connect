f155b76a0a323c8a36552b2306fd0aa303dd7658
Andrew Dryga <andrew@dryga.com>
2023-04-21 11:53:15 -0700

    Fix application supervisor name

--- lib/openid_connect/application.ex
+++ lib/openid_connect/application.ex
@@ -2,7 +2,7 @@ defmodule OpenIDConnect.Application do
   use Application
 
   def start(_type, _args) do
-    opts = [strategy: :one_for_one, name: FzVpn.Supervisor]
+    opts = [strategy: :one_for_one, name: __MODULE__.Supervisor]
     Supervisor.start_link(children(), opts)
   end
 
