3ab6ca37940536949efc3f74d1ff19192ef7c073
Jói Sigurdsson <joi@crankwheel.com>
2023-05-03 17:37:45 +0000

    Upgrade httpoison dependency.

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule OpenIDConnect.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 1.2"},
+      {:httpoison, ">= 1.2.0"},
       {:jason, ">= 1.0.0"},
       {:jose, "~> 1.8"},
       {:earmark, "~> 1.2", only: :dev},
